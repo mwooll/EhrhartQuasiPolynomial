b98bd03e6ca66842e8bfd88c936572a437cdc7eb
mwooll <woolleymark01@gmail.com>
2024-07-26 11:39:07 +0200

    current passing version of the IPF tests

--- refactored/test_integerperiodicfunction.py
+++ refactored/test_integerperiodicfunction.py
@@ -11,24 +11,21 @@ class TestIntegerPeriodicFunction(TestCase):
         self.ipf = self.ipfr([1, 2, 3])
         self.zero = self.ipfr.zero()
 
-    # __init__
     def test_zero(self):
         self.assertEqual(self.ipfr(), self.ipfr([0]))
         self.assertEqual(self.ipfr(), self.zero)
 
     def test_calculate_period(self):
-        self.assertEqual(self.ipf.period, 3)
-        self.assertEqual(self.ipfr([4, 4, 4, 4]).period, 1)
-        self.assertEqual(self.ipfr([1, 2, 1, 2, 1, 2]).period, 2)
+        self.assertEqual(self.ipf.period(), 3)
+        self.assertEqual(self.ipfr([4, 4, 4, 4]).period(), 1)
+        self.assertEqual(self.ipfr([1, 2, 1, 2, 1, 2]).period(), 2)
 
-    # call
     def test_call(self):
         self.assertEqual(self.zero(1), 0)
         self.assertEqual(self.ipf(0), 1)
         self.assertEqual(self.ipf(3), 1)
         self.assertEqual(self.ipf(7), 2)
 
-    # standard dunder methods
     def test_repr(self):
         self.assertEqual(self.ipf.__repr__(),
                          'IntegerPeriodicFunctionElement(Ring of Integer Periodic Functions over Rational Field, [1, 2, 3])')
@@ -40,12 +37,7 @@ class TestIntegerPeriodicFunction(TestCase):
                          + "\n\t2 if k%3 == 1"
                          + "\n\t3 if k%3 == 2")
 
-    # def test_coefficient_repr(self):
-    #     self.assertEqual(self.ipf.coefficient_repr("k"),
-    #                      "IntegerPeriodicFunction([1, 2, 3])(k)")
-    #     self.assertEqual(self.zero.coefficient_repr("k"), "0")
 
-    # math support
     def test_neg(self):
         self.assertEqual(-self.ipf, self.ipfr([-1, -2, -3]))
 
@@ -80,6 +72,5 @@ class TestIntegerPeriodicFunction(TestCase):
                          self.ipfr([1, -2, 3, -1, 2, -3]))
 
 
-
 if __name__ == "__main__":
-    main()   
+    main(failfast=True)   

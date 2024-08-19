4b91fa3ab43e65c443b4edc1e02bac13c4f6307f
mwooll <woolleymark01@gmail.com>
2024-08-19 15:55:57 +0200

    new version of "ehrhart_piecewise"

--- ehrhart_quasi_polynomial/ehrhart_piecewise.py
+++ ehrhart_quasi_polynomial/ehrhart_piecewise.py
@@ -2,47 +2,158 @@ from itertools import combinations_with_replacement
 
 from .ehrhart_quasi_polynomial import ehrhart_quasi_polynomial
 
-import sage.all
+from sage.arith.functions import lcm
 from sage.functions.other import factorial
 from sage.geometry.cone import Cone
 from sage.geometry.polyhedron.constructor import Polyhedron
 from sage.interfaces.gfan import gfan
+from sage.matrix.constructor import Matrix
 from sage.modules.free_module_element import free_module_element
 from sage.rings.polynomial.groebner_fan import PolyhedralFan
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 from sage.rings.rational_field import QQ
-from sage.structure.element import Matrix
 
 
-def piecewise_ehrhart_quasi_polynomial(A, alter=None):
-    """
-    Computes the piecewise Ehrhart Quasi-Polynomial associate with ``A``.
-    Return a piecewise quasi-polynomial where the domains are the maximal cones
-    of the secondary fan of A and the functions on the domains are the 
-    Ehrhart Quasi-Polynomials of A on those maximal cones.
+class PiecewiseEhrhartQuasiPolynomial():
+    def __init__(self, A):
+        self.A = A
+        self.sec_fan = self._secondary_fan()
 
-    EXAMPLES::
-        sage: from ehrhart_quasi_polynomial.ehrhart_piecewise import piecewise_ehrhart_quasi_polynomial
-        sage: A = Matrix([[-1, 0], [0, -1], [1, 1], [0, 1]]) 
-        sage: # eqp = piecewise_ehrhart_quasi_polynomial(A); epq
+        self._projection_generator()
+        self.max_cones, self.qps, R, S = self._compute_piecewise()
+        self.str_var = [str(x) for x in R.gens()]
+        self.amb_dim = self.sec_fan.ambient_dim()
 
-        sage: # epq.domains()
-    """
-    # check if inputs are compatible
-    num_rows = A.nrows()
-    if isinstance(alter, dict):
-        for key, val in alter.items():
-            if key >= num_rows:
-                raise ValueError("'alter' cannot have keys which are larger or equal " +
-                                     f"than the number of rows of `A`, which is {num_rows}")
-            if val not in QQ:
-                raise ValueError(f"values of 'alter' need to be rationals, got {val} of type {type(val)}")
+    def _secondary_fan(self):
+        """
+        Returns the secondary fan associated with ``self.A``
+        For details see the documentation of the gfan software package:
+            https://users-math.au.dk/~jensen/software/gfan/gfan.html
+        """
+        gfan_input = "{" + ", ".join(str(row) for row in self.A.rows()) + "}"
+        return PolyhedralFan(gfan(gfan_input, "secondaryfan"))
 
-    sec_fan = secondary_fan(A)
-    estimated_period = _estimate_period(A, sec_fan)
+    def _compute_piecewise(self):
+        num_variables = self.A.nrows()
+        max_degree = self.sec_fan.lineality_dim()
+    
+        R = PolynomialRing(QQ, "x", num_variables)
+        x_vars = R.gens()
+        S = PolynomialRing(R, "k")
+        k = S.gen()
+    
+        fan_rays = self.sec_fan.rays()
+    
+        max_cones = []
+        quasi_polynomials = []
+        for ray_lists in self.sec_fan.maximal_cones().values():
+            for ray_list in ray_lists:
+                cone = Cone([fan_rays[idx] for idx in ray_list])
+                period = self._estimate_period(cone)
+                max_cones.append(cone)
+    
+                red_cone, dimensions_removed = self._remove_zero_dimensions(cone.rays())
+                actual_num_variables = num_variables - len(dimensions_removed)
+        
+                needed_points = factorial(actual_num_variables + max_degree)//(
+                    factorial(actual_num_variables)*factorial(max_degree) )*period
+                cone_points = self._generate_cone_points(cone, needed_points)
+                term_dict = self._get_term_dict(self.A, cone_points)
+    
+                interpolation_variables = [x for dim, x in enumerate(x_vars)
+                                           if dim not in dimensions_removed]
+                T = PolynomialRing(QQ, names=interpolation_variables)
+                cone_points = [[p for dim, p in enumerate(point)
+                                if dim not in dimensions_removed]
+                               for point in cone_points]
+    
+                cone_poly = 0
+                for degree, terms in term_dict.items():
+                    cone_poly += T.interpolation(degree, cone_points, terms)*k**degree
+    
+                quasi_polynomials.append(cone_poly)
+        return max_cones, quasi_polynomials, R, S
 
-    max_cones, piecewies_qps = _compute_piecewise(A, sec_fan, estimated_period, alter)
-    return max_cones, piecewies_qps
+    def _estimate_period(self, chamber):
+        # TODO: find better estimate for each chamber
+        # since if the polytope is empty in a chamber we could skip all calculations
+        return lcm(a for a in self.A.list() if a)
+
+    def _remove_zero_dimensions(self, rays):
+        zero_dimensions = []
+        for dim, val in enumerate(rays[0]):
+            if val == 0:
+                if all(ray[dim] == 0 for ray in rays[1:]):
+                    zero_dimensions.append(dim)
+        reduced_cone = Cone([[val for dim, val in enumerate(ray)
+                              if dim not in zero_dimensions ]for ray in rays])
+        return reduced_cone, zero_dimensions
+
+    def _generate_cone_points(self, cone, number):
+        vectors = [free_module_element(list(ray)) for ray in cone.rays()]
+
+        points = []
+        points_len = 0
+        index = 1
+        while points_len <= number:
+            new_points = [sum(combi) for combi
+                          in combinations_with_replacement(vectors, index)]
+            points += new_points
+            index += 1
+            points_len += len(new_points)
+        return points[:number]
+
+    def _get_term_dict(self, A_matrix, points):
+        polynomials = []
+        for point in points:
+            polytope = self._create_polytope_from_matrix(point)
+            ehr_poly = ehrhart_quasi_polynomial(polytope.Vrepresentation())
+            polynomials.append(ehr_poly)
+
+        max_degree = max(poly.degree() for poly in polynomials)
+        terms = {d: self._get_terms_of_order(polynomials, d)
+                 for d in range(max_degree + 1)}
+        return terms
+
+    def _create_polytope_from_matrix(self, b):
+        inequalities = [[b[k]] + list(-self.A.rows()[k]) for k in range(self.A.nrows())]
+        return Polyhedron(ieqs = inequalities)
+
+    def _get_terms_of_order(self, polynomials, order):
+        return [poly.coefficients()[order].constants()[0]
+                if poly.degree() >= order else 0 for poly in polynomials]
+
+    def _projection_generator(self):
+        orth_vectors = []
+        for vec in self.sec_fan.fan_dict["ORTH_LINEALITY_SPACE"]:
+            orth_vectors.append(free_module_element([int(val) for val in vec.split(" ")]))
+    
+        lin_vectors = []
+        for vec in self.sec_fan.fan_dict["LINEALITY_SPACE"]:
+            lin_vectors.append(free_module_element([int(val) for val in vec.split(" ")]))
+
+        change_of_basis_matrix = Matrix(orth_vectors + lin_vectors)
+        inverse = change_of_basis_matrix.inverse()
+    
+        def projection(point):
+            new_representation = free_module_element(point)*inverse
+            eval_point = sum(new_representation[k]*o_vec for k, o_vec in enumerate(orth_vectors))
+            return eval_point
+    
+        self.projection = projection
+
+
+    def __call__(self, point):
+        return self.evaluate(point)
+
+    def evaluate(self, point):
+        proj = self.projection(point)
+        for idx, cone in enumerate(self.max_cones):
+            if proj in cone:
+                keywords = {x: proj[k] for k, x in enumerate(self.str_var)}
+                return self.qps[idx](**keywords)
+        return 0
+
 
 def create_polytope_from_matrix(A, b):
     """
@@ -69,110 +180,5 @@ def create_polytope_from_matrix(A, b):
     if A.nrows() != len(b):
         raise ValueError("Dimensions of 'A' and 'b' need to be compatible, need "+
                          f"A.nrows == len(b), but have {A.nrows()} != {len(b)}")
-    return _create_polytope_from_matrix(A, b) 
-
-
-def secondary_fan(A):
-    """
-    Returns the secondary fan of the matrix ``A``.
-    For computational details see the documentation of the gfan software package:
-        https://users-math.au.dk/~jensen/software/gfan/gfan.html
-    """
-    gfan_input = "{" + ", ".join(str(row) for row in A.rows()) + "}"
-    return PolyhedralFan(gfan(gfan_input, "secondaryfan"))
-
-def _estimate_period(A, sec_fan): return 1
-
-def _compute_piecewise(A_matrix, sec_fan, period=1, alter=None):
-    num_variables = A_matrix.nrows()
-    max_degree = sec_fan.lineality_dim()
-
-    R = PolynomialRing(QQ, "x", num_variables)
-    x_vars = R.gens()
-    S = PolynomialRing(R, "k")
-    k = S.gen()
-
-    fan_rays = sec_fan.rays()
-
-    if isinstance(alter, dict):
-        fan_rays = _alter_rays(fan_rays, alter, num_variables)
-
-    max_cones = []
-    quasi_polynomials = []
-    for ray_lists in sec_fan.maximal_cones().values():
-        for ray_list in ray_lists:
-            cone = Cone([fan_rays[idx] for idx in ray_list])
-            max_cones.append(cone)
-
-            red_cone, dimensions_removed = _remove_zero_dimensions(cone.rays())
-            actual_num_variables = num_variables - len(dimensions_removed)
-    
-            needed_points = factorial(actual_num_variables + max_degree)//(
-                factorial(actual_num_variables)*factorial(max_degree) )
-            cone_points = _generate_cone_points(cone, needed_points)
-            term_dict = _get_term_dict(A_matrix, cone_points)
-
-            interpolation_variables = [x for dim, x in enumerate(x_vars)
-                                       if dim not in dimensions_removed]
-            T = PolynomialRing(QQ, names=interpolation_variables)
-            cone_points = [[p for dim, p in enumerate(point)
-                            if dim not in dimensions_removed]
-                           for point in cone_points]
-
-            cone_poly = 0
-            for degree, terms in term_dict.items():
-                cone_poly += T.interpolation(max_degree, cone_points, terms)*k**degree
-
-            quasi_polynomials.append(cone_poly)
-    return max_cones, quasi_polynomials
-
-def _alter_rays(rays, alter):
-    altered_rays = []
-    for ray in rays:
-        altered_ray = ray
-        for key, val in alter.items():
-            altered_ray[key] = val
-        altered_rays.append(altered_ray)
-    return altered_rays
-
-def _remove_zero_dimensions(rays):
-    zero_dimensions = []
-    for dim, val in enumerate(rays[0]):
-        if val == 0:
-            if all(ray[dim] == 0 for ray in rays[1:]):
-                zero_dimensions.append(dim)
-    reduced_cone = Cone([[val for dim, val in enumerate(ray) if dim not in zero_dimensions ]for ray in rays])
-    return reduced_cone, zero_dimensions
-
-def _generate_cone_points(cone, number):
-    vectors = [free_module_element(list(ray)) for ray in cone.rays()]
-
-    points = []
-    points_len = 0
-    index = 1
-    while points_len <= number:
-        new_points = [sum(combi) for combi
-                      in combinations_with_replacement(vectors, index)]
-        points += new_points
-        index += 1
-        points_len += len(new_points)
-    return points[:number]
-
-def _get_term_dict(A_matrix, points):
-    polynomials = []
-    for point in points:
-        polytope = _create_polytope_from_matrix(A_matrix, point)
-        ehr_poly = ehrhart_quasi_polynomial(polytope.Vrepresentation())
-        polynomials.append(ehr_poly)
-
-    max_degree = max(poly.degree() for poly in polynomials)
-    terms = {d: _get_terms_of_order(polynomials, d)for d in range(max_degree + 1)}
-    return terms
-
-def _create_polytope_from_matrix(A, b):
     inequalities = [[b[k]] + list(-A.rows()[k]) for k in range(A.nrows())]
     return Polyhedron(ieqs = inequalities)
-
-def _get_terms_of_order(polynomials, order):
-    return [poly.coefficients()[order].constants()[0]
-            if poly.degree() >= order else 0 for poly in polynomials]
